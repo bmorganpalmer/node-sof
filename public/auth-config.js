15da2fa3b76f6410d9be368deca0dfc1f9688525
Brian Palmer <6209170+bmorganpalmer@users.noreply.github.com>
2025-03-12 15:32:07 -0400

    Create auth-config.js

---
+++ public/auth-config.js
@@ -0,0 +1,13 @@
+// Cognito configuration
+const authConfig = {
+  region: 'us-east-1',
+  userPoolId: 'us-east-1_HBR3XpaTX',
+  userPoolWebClientId: 'o3p0n43e4s8c66medioapfkq5',
+  oauth: {
+    domain: 'us-east-1yeflr48wo.auth.us-east-1.amazoncognito.com',
+    scope: ['email', 'profile', 'openid'],
+    redirectSignIn: window.location.origin,
+    redirectSignOut: window.location.origin,
+    responseType: 'code'
+  }
+};
