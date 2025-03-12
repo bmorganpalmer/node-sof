3bdf259534713caead114b5b5425d1e1384d1eb0
Brian Palmer <6209170+bmorganpalmer@users.noreply.github.com>
2025-03-12 16:24:10 -0400

    Update auth-config.js

--- public/auth-config.js
+++ public/auth-config.js
@@ -1,13 +1,18 @@
 // Cognito configuration
 const authConfig = {
-  region: 'us-east-1',
-  userPoolId: 'us-east-1_HBR3XpaTX',
-  userPoolWebClientId: 'o3p0n43e4s8c66medioapfkq5',
-  oauth: {
-    domain: 'us-east-1yeflr48wo.auth.us-east-1.amazoncognito.com',
-    scope: ['email', 'profile', 'openid'],
-    redirectSignIn: window.location.origin,
-    redirectSignOut: window.location.origin,
-    responseType: 'code'
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_HBR3XpaTX',
+    userPoolWebClientId: 'o3p0n43e4s8c66medioapfkq5',
+    oauth: {
+      domain: 'us-east-1yeflr48wo.auth.us-east-1.amazoncognito.com',
+      scope: ['email', 'profile', 'openid'],
+      redirectSignIn: window.location.origin,
+      redirectSignOut: window.location.origin,
+      responseType: 'code'
+    }
   }
 };
+
+// Make it available globally
+window.authConfig = authConfig;
